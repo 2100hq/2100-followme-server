01e32a7e34c1df2d46476ea86756b806802f749b
Brett <brett@citizenhex.com>
2019-08-26 09:50:22 -0400

    public/getTokenFeed; always returns hidden messages

--- libs/actions/public.js
+++ libs/actions/public.js
@@ -11,6 +11,13 @@ module.exports = (config,{threads,messages}) => {
       echo(...args){
         return args
       },
+      async getTokenFeed(tokenid,start,end){
+        const list = await threads.between(tokenid,start,end)
+        return Promise.map(list,async thread=>{
+          const message = await messages.get(thread.messageid)
+          return hideMessage(message)
+        })
+      },
       async feed(start,end){
         const list = await threads.between(publicFeedId,start,end)
         const ms = await messages.getAll(list.map(x=>x.messageid))
