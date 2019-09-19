1334966beacd61fecc132f39dbeaaac01b95acaf
daywiss <daywiss@gmail.com>
2019-09-19 15:43:24 -0400

    add lazy cache to messages

--- libs/models/init-rethink.js
+++ libs/models/init-rethink.js
@@ -1,10 +1,13 @@
 const Users = require('./users')
 const Threads = require('./threads')
+const LazyCache = require('./lazy-cache')
 const Messages = require('./messages')
 module.exports = async (config,{con},emit=x=>x) => {
   return {
     users:Users.Model(config,await Users.Rethink({table:'users'},con),(...args)=>emit('users',...args)),
     threads:Threads.Model(config,await Threads.Rethink({table:'threads'},con),(...args)=>emit('threads',...args)),
-    messages:Messages.Model(config,await Messages.Rethink({table:'messages'},con),(...args)=>emit('messages',...args)),
+    messages:Messages.Model(config,
+      LazyCache(config,await Messages.Rethink({table:'messages'},con)),
+      (...args)=>emit('messages',...args)),
   }
 }
