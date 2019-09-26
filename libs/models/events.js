d4e4fc6dafa72b5fb3f90668bc92c1b5a274770c
daywiss <daywiss@gmail.com>
2019-09-26 15:31:57 -0400

    fix mlutiple public feed events

--- libs/models/events.js
+++ libs/models/events.js
@@ -2,14 +2,18 @@ const { hideMessage } = require('../utils')
 
 module.exports = (config,libs,emit=x=>x)=>{
 
+  const {publicFeedId} = config
+
   async function write([table,method,data]){
     switch(table){
       case 'notifications':
         return emit('private',['notifications',data.id],data,data.userid)
       case 'threads':{
-        const message = await libs.messages.get(data.messageid)
-        const hidden = hideMessage(message)
-        emit('public',['feed',data.messageid],hidden)
+        if(data.threadid === publicFeedId){
+          const message = await libs.messages.get(data.messageid)
+          const hidden = hideMessage(message)
+          emit('public',['feed',data.messageid],hidden)
+        }
       }
     }
   }
